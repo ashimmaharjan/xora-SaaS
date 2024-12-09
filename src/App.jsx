1702c901461a6fa00389bbee4d53aaf3317345cc
Ashim Maharjan <ciferulquiorra008@gmail.com>
2024-12-09 17:50:16 +1100

    testimonial item created as reusable component and testimonials section design implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
+import Faq from "./sections/Faq.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       <Header />
       <Hero />
       <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
     </main>
   );
 };
